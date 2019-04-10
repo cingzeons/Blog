c3daab77f4e5fa4b00a19781fad3261f32039ff7
cingzeon <cingzeon@gmail.com>
2019-04-10 10:08:00 +0800

    updata

---
+++ build/precache-manifest.a96b5b8e1d6bff899711e0ec387adce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f57bfb29339f62ab972",
+    "url": "./static/css/main.3b7081fa.chunk.css"
+  },
+  {
+    "revision": "8f57bfb29339f62ab972",
+    "url": "./static/js/main.3a85f1f4.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "./static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c4a803f6f2d41aa4a7e47b19a895584",
+    "url": "./index.html"
+  }
+];
